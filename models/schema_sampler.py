8af0fa9653db7ef8af627fe51f7fd1d3ee83fd20
arthur-plautz <atr.plautz@gmail.com>
2024-02-12 16:06:20 -0300

    Enhancing Validation and Sampling

--- models/schema_sampler.py
+++ models/schema_sampler.py
@@ -4,20 +4,52 @@ from models.schema_validator import SchemaValidator
 
 class SchemaSampler(SchemaValidator):
     def __init__(self, exp, method):
-        self.random_margin = 10
         super().__init__(exp, method)
 
-    def _rand(self):
-        return np.random.randint(0, self.random_margin)
-
-    def _generate_field(self, field_type):
-        field_types = {
-            "str": str(self._rand()),
-            "int": int(self._rand()),
-            "float": float(self._rand()/(self._rand()+1)),
-            "list": [self._rand() for _ in range(self._rand())]
+    def _generate_value(self, value_type, value_range=(0,10)):
+        s, e = value_range
+        print(s,e)
+        value_types = {
+            "str": "-",
+            "int": self._rand(s, e),
+            "float": sum([0.1 for _ in range(s, self._rand(s, e))]),
         }
-        return field_types.get(field_type)
+        return value_types.get(value_type)
+
+    def _generate_list(self, field):
+        expected = self.schema.get(field)
+        l = expected.get("length", self._rand())
+        t = expected.get("items_type", "str")
+        return [self._generate_value(t) for _ in range(l)]
+
+    def _generate_numeric(self, field):
+        expected = self.schema.get(field)
+        t = expected.get("type")
+        r = expected.get("range", (0,10))
+        return self._generate_value(t, r)
+
+    def _generate_str(self, field):
+        expected = self.schema.get(field)
+        o = expected.get("options", [])
+        if o:
+            return o[self._rand(0, len(o))]
+        else:
+            return self._generate_value("str")
+
+    def _generate_field(self, field):
+        type_generation = {
+            "str": self._generate_str,
+            "int": self._generate_numeric,
+            "float": self._generate_numeric,
+            "list": self._generate_list
+        }
+        expected = self.schema.get(field)
+        expected_type = expected.get("type")
+        generation = type_generation.get(expected_type)
+        return generation(field)
+
+    def _rand(self, min=0, max=10):
+        return np.random.randint(min, max)
 
     def _save(self, data, path):
         with open(path, 'w') as yaml_file:
@@ -27,8 +59,8 @@ class SchemaSampler(SchemaValidator):
         path = path if path else f"data/sample{self._rand()}.yml"
 
         sample = {}
-        for k, v in self.schema.items():
-            sample[k] = self._generate_field(v)
+        for field in self.schema.keys():
+            sample[field] = self._generate_field(field)
 
         sample_path = self._format_path(path)
         self._save(
--- models/schema_validator.py
+++ models/schema_validator.py
@@ -10,6 +10,48 @@ class SchemaValidator:
         self.schema_path = self._format_path("data/schema.yml")
         self.schema = self._read(self.schema_path)
 
+    def _validate_list(self, field, value):
+        expected = self.schema.get(field)
+        if "length" in expected.keys():
+            l = expected.get("length")
+            if len(value) != l:
+               raise Exception(f"Field {field} must have length {l}. Value: {value}")
+        if "items_type" in expected.keys():
+            t = expected.get("items_type")
+            for i in value:
+                if isinstance(i, eval(t)):
+                    raise Exception(f"Field {field} has incorrect item type [{type(i)}], expected [{t}].")
+
+    def _validate_numeric(self, field, value):
+        expected = self.schema.get(field)
+        if "range" in expected.keys():
+            r = expected.get("range")
+            if value < r[0] or value > r[1]:
+               raise Exception(f"Field {field} must be in range {r}. Value: {value}")
+
+    def _validate_str(self, field, value):
+        expected = self.schema.get(field)
+        if "options" in expected.keys():
+           o = expected.get("options")
+           if value not in o:
+               raise Exception(f"Field {field} must be on of {o}. Value: {value}")
+
+    def _validate_field(self, field, value):
+        type_validation = {
+            "str": self._validate_str,
+            "int": self._validate_numeric,
+            "float": self._validate_numeric,
+            "list": self._validate_list
+        }
+        expected = self.schema.get(field)
+        expected_type = expected.get("type")
+
+        if not isinstance(value, eval(expected_type)):
+            raise Exception(f"Field {field} has incorrect type [{type(value)}], expected [{expected_type}].")
+        else:
+            validation = type_validation.get(expected_type)
+            validation(field, value)
+
     def _format_path(self, path):
         return f"{self.base_path}/{path}"
 
@@ -25,7 +67,4 @@ class SchemaValidator:
             self._format_path(path)
         )
         for k, v in data.items():
-            expected = self.schema.get(k)
-            if not isinstance(v, eval(expected)):
-                raise Exception(f"Field {k} has incorrect type [{type(v)}], expected [{expected}].")
-
+            self._validate_field(k, v)
